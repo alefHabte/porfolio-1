52da1ec6c35d052dae47cec306f87d4d89bd7092
Alef <alefalmawossen@gmail.com>
2022-12-11 20:50:16 +0300

    after resolve

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,12 @@
 module.exports = {
   plugins: [
     {
-      resolve: "Portfolio",
+      resolve: "gatsby-theme-portfolio-minimal",
       options: {
         siteUrl: "https://gatsby-starter-portfolio-minimal-theme.netlify.app/", // Used for sitemap generation
         manifestSettings: {
           favicon: "./content/images/me.jpg", // Path is relative to the root
-          siteName: "Portfolio", // Used in manifest.json
+          siteName: "My Minimal Portfolio", // Used in manifest.json
           shortName: "Portfolio", // Used in manifest.json
           startUrl: "/", // Used in manifest.json
           backgroundColor: "#FFFFFF", // Used in manifest.json
